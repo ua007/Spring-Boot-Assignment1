3035e5f1abab6fb2a57fc01a29380b45de3e5914
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-29 17:07:43 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/userService/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/userService/service/TrackServiceImpl.java
@@ -22,7 +22,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track saveUser(Track track) throws TrackAlreadyExistException{
+    public Track saveTrack(Track track) throws TrackAlreadyExistException{
 
         if(trackRepository.existsById(track.getId()))
         {
@@ -33,7 +33,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public List<Track> deleteUser(int id) throws TrackNotFoundException{
+    public List<Track> deleteTrack(int id) throws TrackNotFoundException{
         if(trackRepository.existsById(id))
         {
             throw new TrackNotFoundException("Track not found");
@@ -43,7 +43,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track updateUser(int id, String name, String comment) {
+    public Track updateTrack(int id, String name, String comment) {
         Optional<Track> user= trackRepository.findById(id);
         Track track =user.get();
         track.setName(name);
@@ -54,7 +54,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public List<Track> getAllUsers() {
+    public List<Track> getAllTracks() {
 
         return trackRepository.findAll();
     }
